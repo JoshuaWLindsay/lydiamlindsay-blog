d37c8dd514038864faa8f7a96d835a2a30757824
Joshua Lindsay <99047296+JoshuaWLindsay@users.noreply.github.com>
2024-04-09 16:39:36 -0500

    added Vercel SpeedInsights to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./styles.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { BLOG_TITLE } from "@/constants";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
